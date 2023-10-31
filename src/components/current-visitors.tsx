ab5b7973f0b63a51d94fa3193a413650b6de01bf
Piyush Yadav <84402719+piyushyadav0191@users.noreply.github.com>
2023-10-31 18:01:33 +0530

    Update current-visitors.tsx

--- src/components/current-visitors.tsx
+++ src/components/current-visitors.tsx
@@ -25,7 +25,7 @@ const CurrentVisitors = () => {
             <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
             <span className='relative inline-flex h-3 w-3 rounded-full bg-green-500' />
           </span>
-          {data.visitors} current visitors
+          {data.visitors} I'm watching you
         </>
       ) : (
         <Skeleton
