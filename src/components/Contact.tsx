8a8c23fb65bf2e24e6785d2a9af57f199e7b1106
Piyush Yadav <piyushyadav0191@gmail.com>
2023-11-01 08:59:54 +0530

    removed selection in mobile view

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,7 +17,67 @@ const Contact = () => {
           don't just say hello.
         </Link>
       </p>
-      <div className="cc-bg blur-background mx-auto mt-8 w-4/5 w-full rounded-lg border bg-transparent p-8 text-white shadow-lg md:max-w-4xl">
+      <br />
+
+      <div>
+        <p className="sm:text-lg">There's a few ways you can get it touch:</p>
+        <br />
+        <div className="flex justify-between items-center  flex-row gap-6 max-w-xs mb-2">
+          <div className="flex flex-row gap-6">
+            <p className="sm:text-lg">Email</p>
+          </div>
+          <div className="flex flex-row gap-6">
+            <a
+              href="mailto:piyushyadav0191@gmail.com?subject=Hi%20Piyush!"
+              target="_blank"
+              className="sm:text-lg text-gray-600 font-semibold hover:text-red-600"
+            >
+              piyushyadav0191@gmail.com
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-between items-center  flex-row gap-6 max-w-xs mb-2">
+          <div className="flex flex-row gap-6">
+            <p className="sm:text-lg">linkedin</p>
+          </div>
+          <div className="flex flex-row gap-6">
+            <a
+              href="https://www.linkedin.com/in/piyushyadav0191/"
+              className="sm:text-lg text-gray-600 font-semibold hover:text-sky-600"
+            >
+              piyushyadav0191
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-between items-center  flex-row gap-6 max-w-xs mb-2">
+          <div className="flex flex-row gap-6">
+            <p className="sm:text-lg">Medium</p>
+          </div>
+          <div className="flex flex-row gap-6">
+            <a
+              href="https://medium.com/@piyushyadav0191"
+              className="sm:text-lg text-gray-600 font-semibold hover:text-violet-600"
+            >
+              piyushyadav0191
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-between items-center  flex-row gap-6 max-w-xs mb-2">
+          <div className="flex flex-row gap-6">
+            <p className="sm:text-lg">Ask me Anything</p>
+          </div>
+          <div className="flex flex-row gap-6">
+            <a
+              href="https://github.com/piyushyadav0191"
+              className="sm:text-lg text-gray-600 font-semibold hover:text-pink-600"
+            >
+              on Github
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="cc-bg blur-background mx-auto mt-12 w-4/5 w-full rounded-lg border bg-transparent p-8 text-white shadow-lg md:max-w-4xl">
         <div className="flex items-center justify-between">
           <div>
             <h2 className="mb-2 text-2xl font-bold text-black dark:text-white">
