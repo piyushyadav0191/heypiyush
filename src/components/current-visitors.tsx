da18805e82f18608fe859be51aaaaf6aba74b247
Piyush Yadav <piyushyadav0191@gmail.com>
2023-11-09 20:18:41 +0530

    added tech stack

--- src/components/current-visitors.tsx
+++ src/components/current-visitors.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import useSWR from 'swr'
-
-import { Skeleton } from '@/components/ui'
-import fetcher from '@/lib/fetcher'
-import { type Analytics } from '@/types'
-
 const CurrentVisitors = () => {
-  const { data } = useSWR<Analytics>('/api/analytics', fetcher, {
-    refreshInterval: 30_000 // refresh every 30 seconds
-  })
+  // const { data } = useSWR<Analytics>('/api/analytics', fetcher, {
+  //   refreshInterval: 30_000 // refresh every 30 seconds
+  // })
 
   return (
     <a
@@ -19,20 +13,15 @@ const CurrentVisitors = () => {
       target='_blank'
       aria-label='Current visitors'
     >
-      {data ? (
-        <>
-          <span className='relative flex h-3 w-3'>
-            <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
-            <span className='relative inline-flex h-3 w-3 rounded-full bg-green-500' />
-          </span>
-          {data.visitors} I'm watching you
-        </>
-      ) : (
-        <Skeleton
-          className='h-5 w-32 rounded-md'
-          data-testid='skeleton-loader'
-        />
-      )}
+
+      <>
+        <span className='relative flex h-3 w-3'>
+          <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
+          <span className='relative inline-flex h-3 w-3 rounded-full bg-green-500' />
+        </span>
+        I'm watching you
+      </>
+
     </a>
   )
 }
