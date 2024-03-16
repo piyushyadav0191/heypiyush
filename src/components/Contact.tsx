6a91f6d767324937421c78a9c3087804f5d361bc
Piyush yadav <piyushyadav0191@gmail.com>
2024-03-16 09:35:09 +0530

    removed ts config part and resume

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -77,66 +77,7 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className="cc-bg blur-background mx-auto mt-12 w-4/5 w-full rounded-lg border bg-transparent p-8 text-white shadow-lg ">
-        <div className="flex items-center justify-between">
-          <div>
-            <h2 className="mb-2 text-2xl font-bold text-black dark:text-white">
-              Piyush Yadav
-            </h2>
-            <p className="mb-4 text-sm text-black dark:text-white">
-              Software Engineer
-            </p>
-            <button className="rounded border bg-inherit px-4 py-2 dark:border-white text-black shadow transition-colors duration-200 hover:bg-gray-100 hover:text-sky-500 dark:text-white dark:hover:text-black">
-              <Link
-                href={`/pdf/resume.pdf`}
-                target="_blank"
-                className="animate-pulse"
-              >
-                Resume
-              </Link>
-            </button>
-          </div>
-
-          <div className="flex space-x-4">
-            <a
-              href="https://github.com/piyushyadav0191"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-gray-200 "
-            >
-              {theme === "dark" ? (
-                <Image src={"/images/contact/github-white.svg"} height={30} width={30} alt="github" />
-              ) : (
-                <Image src={"/images/contact/github.svg"} height={30} width={30} alt="github" />
-              )}
-            </a>
-            <a
-              href="https://www.linkedin.com/in/piyushyadav0191"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-gray-200"
-            >
-              {theme === "dark" ? (
-                <Image src={"/images/contact/linkedin-white.svg"} height={30} width={30} alt="github" />
-              ) : (
-                <Image src={"/images/contact/linkedin.svg"} height={30} width={30} alt="github" />
-              )}
-            </a>
-            <a
-              href="https://instagram.com/iim.piyush"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-gray-200"
-            >
-              {theme === "dark" ? (
-                <Image src={"/images/contact/instagram-white.svg"} height={30} width={30} alt="github" />
-              ) : (
-                <Image src={"/images/contact/instagram.svg"} height={30} width={30} alt="github" />
-              )}
-            </a>
-          </div>
-        </div>
-      </div>
+      {/* form  */}
     </div>
   );
 };
