474b23ad080b8773f8307ad7433d9cf42150f0b8
Piyush Yadav <piyushyadav0191@gmail.com>
2024-04-01 16:05:33 +0530

    Refactor experience and education sections

--- src/app/experience/_components/Experience.tsx
+++ src/app/experience/_components/Experience.tsx
@@ -12,72 +12,58 @@ const Experience = () => {
       <h2 className="mb-6 text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
         Work Experience
       </h2>
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-      >
-        {experiences.length > 0 &&
-          experiences.map((experience, index) => (
-            <Card key={index}>
-              <div className="relative z-10 flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                <Image
-                  src={experience.logo}
-                  alt={experience.company}
-                  className="w-8 h-8"
-                  width={32}
-                  height={32}
-                />
-              </div>
-              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <Card.Link href={experience.link.url}>
-                  {experience.title} at {experience.company} - {experience.location}
-                </Card.Link>
-              </h2>
-              <p className="text-sm text-zinc-600 dark:text-zinc-400">
-                {experience.date}
-              </p>
-              <div className="relative z-10 mt-2 text-sm text-zinc-700 dark:text-zinc-400">
-                {experience.description.map((item, index) => (
-                  <li className="ml-4 list-disc" key={`description-${index}`}>
-                    {item}
-                  </li>
-                ))}
-              </div>
-              <p className="relative z-10 flex mt-6 text-sm font-medium transition text-zinc-400 group-hover:text-sky-500 dark:text-zinc-200">
-                <IconLink className="flex-none w-6 h-6" />
-                <span className="ml-2">{experience.link.label}</span>
-              </p>
-            </Card>
-          ))}
-      </ul>
-
-      <div className="relative max-w-lg mx-auto mt-24 lg:max-w-7xl">
-        <div>
-          <h2 className="mb-6 text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
-            Education
-          </h2>
-        </div>
-        <div className="grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
-          {siteMetadata.experience.education.map((item, index) => (
-            <Card key={`education-${index}`}>
-              <p className="text-sm text-zinc-600 dark:text-zinc-400">
-                {item.startDate} - {item.endDate}
-              </p>
-              <div className="block mt-2">
-                <p className="text-xl font-semibold text-zinc-800 dark:text-zinc-100">
-                  {item.degree}
-                </p>
-                <p className="text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                  {item.schoolName}
-                </p>
-                <p className="mt-3 text-base text-zinc-600 dark:text-zinc-400">
-                  {item.description}
-                </p>
-              </div>
-            </Card>
-          ))}
-        </div>
+      <section>
+      <div className="prose prose-neutral dark:prose-invert">
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Codemate</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          SDE - I - Full Stack Engineer - Nov 2023 - Present
+        </p>
+        <ul>
+          <li>
+          Designed and implemented full-stack services using Node.js, Flask, and Azure, enhancing the functionality and reliability of the system
+          </li>
+          <li>
+          Collaborated with cross-functional teams to create a user-friendly dashboard, integrating front-end with OpenAI and ensuring seamless back-end functionality.
+          </li>
+          <li>
+          Conducted regular code reviews, providing constructive feedback to maintain high code quality standards, and ensuring scalability and maintainability
+          </li>
+        </ul>
+      
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">One Oath</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Full Stack Developer Intern - July 2023 - Oct 2023
+        </p>
+        <ul>
+          <li>
+          Spearheaded the development of microservices and user interface components, resulting in a 30% increase in user engagement for the client’s personality development system.
+          </li>
+          <li>
+          Developed a CI/CD workflow to automatically perform a set of actions daily on a product in development in order to decrease time needed for team members to identify and fix bugs/issues.
+          </li>
+          <li>
+          Collaborated closely with the AI core team to strategically integrate Python backend components, ensuring the seamless operation of critical system functions.
+          </li>
+        </ul>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">AAA Intergalactic – Infrastructure</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+        Web Developer Intern - April 2023 - July 2023
+        </p>
+        <ul>
+          <li>
+          Orchestrated the creation of static websites for clients, accompanied by the development of microservices for seamless website operation
+          </li>
+          <li>
+          Played a pivotal role in shaping architecture plans, UI designs, and feature development, contributing to a 15% increase in project success rate
+          </li>
+          <li>
+          Drove the development of static websites and streamlined microservices, leading to a remarkable 20% decrease in project timelines
+          </li>
+        </ul>
       </div>
+    </section>
     </>
   );
 };
