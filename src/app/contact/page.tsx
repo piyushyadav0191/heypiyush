647b8d4365f58280667bc860531fcb0a2452da92
Piyush Yadav <piyushyadav0191@gmail.com>
2024-04-12 18:59:34 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,16 @@
 import Contact from '@/components/Contact'
 import PageTitle from '@/components/page-title'
+import type {Metadata} from "next"
 
 export const runtime = 'edge'
 export const title = 'Contact'
 export const description = 'A student who loves web development.'
+
+export const metadata: Metadata = {
+  title: "Contact - Piyush Yadav",
+    description:
+    "Piyush Yadav is a full-stack developer in India. He is creative and passionate about design and technology so he always try to craft great-looking software products.",
+};
 
 const ContactPage = () => {
   return (
