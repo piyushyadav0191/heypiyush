3802db8bb5b5259835d738252c5090fede2ae5dc
Piyush Yadav <piyushyadav0191@gmail.com>
2024-04-24 09:03:07 +0530

    projects updated

--- src/content/AllProject.ts
+++ src/content/AllProject.ts
@@ -21,12 +21,123 @@ export const allProjects: Project[] = [
   {
     id: "1",
     type: "Project",
+    name: "Research PDFs Decoder",
+    description: "Research Pdf Decoder is a Full Stack side project which focus on extracting important notes from any Research papers PDF. It also capable of chatting with PDFs",
+    homepage: "",
+    github: "https://github.com/piyushyadav0191/Research-PDFs-Decoder",
+    icon: "/images/projects/icons/blog.png",
+    image: "/images/projects/formulator.png",
+    repo: "AI-Formulator",
+    techstack: [
+      {
+        id: "1",
+        type: "Techstack",
+        label: "NextJS",
+      },
+      {
+        id: "2",
+        type: "Techstack",
+        label: "Typescript",
+      },
+      {
+        id: "3",
+        type: "Techstack",
+        label: "NodeJS",
+      },
+      {
+        id: "4",
+        type: "Techstack",
+        label: "Express",
+      },
+      {
+        id: "5",
+        type: "Techstack",
+        label: "Open AI",
+      },
+      {
+        id: "6",
+        type: "Techstack",
+        label: "Docker",
+      },
+      {
+        id: "7",
+        type: "Techstack",
+        label: "langchain",
+      },
+      {
+        id: "8",
+        type: "Techstack",
+        label: "Supabase",
+      },
+    ],
+  },
+  {
+    id: "2",
+    type: "Project",
+    name: "keepDraw",
+    description: "KeepDraw is a typescript powered Full Stack Application where user can create real time Boards and draw on it with their organization's members",
+    homepage: "https://keep-draw.vercel.app/",
+    github: "https://github.com/piyushyadav0191/KeepDraw",
+    icon: "/images/projects/icons/farewello.png",
+    image: "/images/projects/farewello.png",
+    repo: "keepDraw",
+    techstack: [
+      {
+        id: "1",
+        type: "Techstack",
+        label: "NextJS",
+      },
+      {
+        id: "2",
+        type: "Techstack",
+        label: "Typescript",
+      },
+      {
+        id: "3",
+        type: "Techstack",
+        label: "Shadcn UI",
+      },
+      {
+        id: "4",
+        type: "Techstack",
+        label: "Clerk",
+      },
+      {
+        id: "5",
+        type: "Techstack",
+        label: "LiveBlocks",
+      },
+      {
+        id: "6",
+        type: "Techstack",
+        label: "Convex",
+      },
+      {
+        id: "7",
+        type: "Techstack",
+        label: "Zunstand",
+      },
+      {
+        id: "8",
+        type: "Techstack",
+        label: "TailwindCSS",
+      },
+      {
+        id: "9",
+        type: "Techstack",
+        label: "Plausible",
+      },
+    ],
+  },
+  {
+    id: "3",
+    type: "Project",
     name: "AI Formulator",
     description: "Create your forms in seconds with the help of AI",
     homepage: "https://aiformulator.vercel.app/",
     github: "https://github.com/piyushyadav0191/AI-Formulator",
-    icon: "/images/projects/icons/blog.png",
-    image: "/images/projects/formulator.png",
+    icon: "/images/projects/icons/reform.png",
+    image: "/images/projects/reform.png",
     repo: "AI-Formulator",
     techstack: [
       {
@@ -77,41 +188,7 @@ export const allProjects: Project[] = [
     ],
   },
   {
-    id: "2",
-    type: "Project",
-    name: "Farewello",
-    description: "Holiday management for mid-sized Organization",
-    homepage: "https://farewello.vercel.app/",
-    github: "https://github.com/piyushyadav0191/Farewello",
-    icon: "/images/projects/icons/farewello.png",
-    image: "/images/projects/farewello.png",
-    repo: "Farewello",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "NextJS + TS + Node",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "TailwindCSS",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "Shadcn UI",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "Next Auth",
-      },
-      
-    ],
-  },
-  {
-    id: "3",
+    id: "4",
     type: "Project",
     name: "Reform",
     description:
@@ -156,7 +233,7 @@ export const allProjects: Project[] = [
   },
   
   {
-    id: "4",
+    id: "5",
     type: "Project",
     name: "Quesgen Pro",
     description:
@@ -210,7 +287,7 @@ export const allProjects: Project[] = [
     ],
   },
   {
-    id: "5",
+    id: "6",
     type: "Project",
     name: "Issues tracker",
     description: "Create and track your issues easily with this application.",
@@ -248,166 +325,7 @@ export const allProjects: Project[] = [
     ],
   },
   {
-    id: "6",
-    type: "Project",
-    name: "Queue Overflow",
-    description: "A place where you can ask questions and give answers",
-    homepage: "https://queueoverflow-pro.vercel.app/",
-    github: "https://github.com/piyushyadav0191/Queue-Overflow",
-    icon: "/images/projects/icons/queue.png",
-    image: "/images/projects/queue.png",
-    repo: "Queue-Overflow",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "NextJS + TS + Node",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "TailwindCSS",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "Shadcn UI",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "Server Actions",
-      },
-      {
-        id: "5",
-        type: "Techstack",
-        label: "MongoDB",
-      },
-      {
-        id: "6",
-        type: "Techstack",
-        label: "Clerk",
-      },
-      {
-        id: "7",
-        type: "Techstack",
-        label: "Web hooks",
-      },
-    ],
-  },
-  {
     id: "7",
-    type: "Project",
-    name: "Whatsapp Clone",
-    description:
-      "Whatsapp clone with all features from individual chats to group chat and chat to video call",
-    homepage: "https://whatsapp-clone-fs.vercel.app/login",
-    github: "https://github.com/piyushyadav0191/Full-Stack-Whatsapp-Clone",
-    icon: "/images/projects/icons/chat.png",
-    image: "/images/projects/whatsapp.png",
-    repo: "Full-Stack-Whatsapp-Clone",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "ReactJS",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "NodeJS",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "MongoDB",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "Socket.io",
-      },
-      {
-        id: "5",
-        type: "Techstack",
-        label: "WebRTC",
-      },
-      {
-        id: "6",
-        type: "Techstack",
-        label: "Redux Toolkit",
-      },
-    ],
-  },
-  {
-    id: "8",
-    type: "Project",
-    name: "Modern Ecommerce",
-    description: "E commerce website made with latest tech",
-    homepage: "https://next-ecommerce-puce-five.vercel.app/",
-    github: "https://github.com/piyushyadav0191/Modern-E-Commerce-site",
-    icon: "/images/projects/icons/shop.png",
-    image: "/images/projects/ecommerce.png",
-    repo: "Modern-E-Commerce-site",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "NextJS + TS + Node",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "Stripe",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "Zustand",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "TailwindCSS",
-      },
-      {
-        id: "5",
-        type: "Techstack",
-        label: "Framer motion",
-      },
-    ],
-  },
-  {
-    id: "9",
-    type: "Project",
-    name: "Modern Blog",
-    description:
-      "A blog app built with Next.js and Sanity.io would be a web application that utilizes the benefits of both technologies to create a dynamic and easily maintainable blog",
-    homepage: "https://blogwithsanity-piyushyadav0191.vercel.app/",
-    github: "https://github.com/piyushyadav0191/full-stack-next-blog",
-    icon: "/images/projects/icons/blog.png",
-    image: "/images/projects/blog.png",
-    repo: "full-stack-next-blog",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "NextJS + TS + Node",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "Sanity.io",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "TailwindCSS",
-      },
-    ],
-  },
-  {
-    id: "10",
     type: "Project",
     name: "Code with us Editor",
     description:
@@ -437,102 +355,6 @@ export const allProjects: Project[] = [
         id: "4",
         type: "Techstack",
         label: "Express",
-      },
-    ],
-  },
-  {
-    id: "11",
-    type: "Project",
-    name: "Dashboard for Ecommerce",
-    description: "Dashbaord to manage your ecommerce website with all features",
-    homepage: "https://my-eccomerce-dashboard.netlify.app/",
-    github: "https://github.com/piyushyadav0191/e-commerce-dashboard",
-    icon: "/images/projects/icons/dashboard.png",
-    image: "/images/projects/dashboard.png",
-    repo: "e-commerce-dashboard",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "ReactJS",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "NodeJS",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "MongoDB",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "Express",
-      },
-    ],
-  },
-  {
-    id: "12",
-    type: "Project",
-    name: "Darking Horse",
-    description: "Colorful Dark theme for Vs code",
-    homepage:
-      "https://marketplace.visualstudio.com/items?itemName=piyush-yadav-india.darking-horse",
-    github: "https://github.com/piyushyadav0191/Darking-horse",
-    icon: "/images/projects/icons/theme.png",
-    image: "/images/projects/theme.png",
-    repo: "Darking-horse",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "Vs Code",
-      },
-    ],
-  },
-  {
-    id: "13",
-    type: "Project",
-    name: "Share here",
-    description:
-      "Shared is a comment system which you can use to add it in your blog or any website where you need comments, reviews etc.",
-    homepage: "https://shared-client.vercel.app/",
-    github: "https://github.com/piyushyadav0191/share-here",
-    icon: "/images/projects/icons/comment.png",
-    image: "/images/projects/comment.png",
-    repo: "share-here",
-    techstack: [
-      {
-        id: "1",
-        type: "Techstack",
-        label: "NextJS + TS",
-      },
-      {
-        id: "2",
-        type: "Techstack",
-        label: "NodeJS",
-      },
-      {
-        id: "3",
-        type: "Techstack",
-        label: "MongoDB",
-      },
-      {
-        id: "4",
-        type: "Techstack",
-        label: "Express",
-      },
-      {
-        id: "5",
-        type: "Techstack",
-        label: "Zustand",
-      },
-      {
-        id: "6",
-        type: "Techstack",
-        label: "TailwindCSS",
       },
     ],
   },
