4f276b773b19f879e0410459a9fe105a3379509e
Piyush yadav <piyushyadav0191@gmail.com>
2024-08-12 10:13:08 +0530

    Add: Beautiful cursor effect around profile picture

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,8 @@
 import Image from '@/components/mdx/image'
 import { HERO_LINKS } from '@/config/links'
 import Link from 'next/link'
-
+import { SVGProps } from 'react'
+import { Cursor } from './ui/cursor'
 const Hero = () => {
   return (
     <div className='space-y-6 md:my-16'>
@@ -38,6 +39,27 @@ const Hero = () => {
           </p>
         </div>
         <div className='h-15 w-15 relative md:h-64 md:w-64'>
+           <div className='overflow-hidden'>
+        <Cursor
+          attachToParent
+          variants={{
+            initial: { scale: 0.3, opacity: 0 },
+            animate: { scale: 1, opacity: 1 },
+            exit: { scale: 0.3, opacity: 0 },
+          }}
+          transition={{
+            ease: 'easeInOut',
+            duration: 0.15,
+          }}
+          className='left-12 top-4'
+        >
+          <div>
+            <MouseIcon className='h-6 w-6' />
+            <div className='ml-4 mt-1 rounded-[4px] bg-blue-500 px-2 py-0.5 text-neutral-50'>
+              Piyush Yadav
+            </div>
+          </div>
+        </Cursor>
           <Image
             src='/images/profile.jpg'
             className='rounded-full'
@@ -47,6 +69,7 @@ const Hero = () => {
             loading='eager'
             priority
           />
+      </div>
           <div className='absolute inset-0 -z-10 bg-gradient-to-tl from-purple-700 to-orange-700 opacity-0 blur-2xl md:opacity-50' />
         </div>
       </div>
@@ -68,3 +91,32 @@ const Hero = () => {
 }
 
 export default Hero
+
+const MouseIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={26}
+      height={31}
+      fill='none'
+      {...props}
+    >
+      <g clipPath='url(#a)'>
+        <path
+          fill={'#2563eb'}
+          fillRule='evenodd'
+          stroke={'#fff'}
+          strokeLinecap='square'
+          strokeWidth={2}
+          d='M21.993 14.425 2.549 2.935l4.444 23.108 4.653-10.002z'
+          clipRule='evenodd'
+        />
+      </g>
+      <defs>
+        <clipPath id='a'>
+          <path fill={'#2563eb'} d='M0 0h26v31H0z' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
