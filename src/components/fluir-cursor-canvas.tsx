5296c4a0de8c9c7785c8cec3f7ba6258653c86b1
Piyush Yadav <piyushyadav0191@gmail.com>
2024-11-04 19:30:26 +0530

    fix: able to click on dom functions

--- src/components/fluir-cursor-canvas.tsx
+++ src/components/fluir-cursor-canvas.tsx
@@ -9,8 +9,8 @@ const FluidCursor = () => {
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 z-">
-      <canvas id="fluid" className="w-screen h-screen" />
+    <div className="fixed top-0 left-0 -z-10">
+      <canvas id="fluid" className="w-screen h-screen pointer-events-none" />
     </div>
   );
 };
